07fdc5027cd7a39adbb120f3dcdbe8e767963fe2
Slava Drozd <qscokmvbn@mail.ru>
2023-08-01 07:57:34 +0300

    Добавьте файлы проекта.

---
+++ DZ_13/Pair.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_13
+{
+    public class Pair<S, T>
+    {
+        public Pair(S first, T second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public S First { get; }
+        public T Second { get; }
+    }
+
+    public class ComparablePair<T, U> : IComparable<ComparablePair<T, U>>
+        where T : IComparable<T>
+        where U : IComparable<U>
+    {
+        public ComparablePair(T first, U second)
+        {
+            First = first;
+            Second = second;
+        }
+        public T First { get; }
+        public U Second { get; }
+
+        public int CompareTo(ComparablePair<T, U> other)
+        {
+            if (other == null) return 1;
+            int compareFirst = First.CompareTo(other.First);
+            if (compareFirst != 0) return compareFirst;
+            int compareSecond = Second.CompareTo(other.Second);
+            return compareSecond;
+        }
+    }
+}
---
+++ DZ_13/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DZ_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var pair1 = new Pair<int, string>(777, "MEEAT");
+            var pair2 = new Pair<double, bool>(0.3333333333333, true);
+            var pair3 = new Pair<char, char>('G', 'G');
+
+
+            Console.WriteLine($"Pair 1: ({pair1.First}, {pair1.Second})");
+            Console.WriteLine($"Pair 2: ({pair2.First}, {pair2.Second})");
+            Console.WriteLine($"Pair 3: ({pair3.First}, {pair3.Second})");
+
+
+        }
+    }
+}
